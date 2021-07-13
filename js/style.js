3d37b424f21c7e1bb7b63e23d4747cd5eeb20270
Mohammad Sakib Hossain <40665494+shfsakib@users.noreply.github.com>
2021-07-13 21:55:55 +0600

    top head done

---
+++ js/style.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    //Menu hamburger animation start
+    const menuBtn = $('.menu-btn');
+    const hamburger = $(".menu-btn_burger");
+    const nav = $(".bottom-header-row");
+    let showMenu = false;
+    menuBtn.click(function () {
+        if (!showMenu) {
+            nav.css('display', 'block');
+            setTimeout(function () {
+                nav.css('opacity', '0.9');
+            }, 300)
+            showMenu = true;
+        } else {
+            nav.css('opacity', '0');
+            setTimeout(function () {
+                nav.css('display', 'block');
+            }, 300)
+            showMenu = false;
+        }
+    });
+    $('.closeMenu').click(function () {
+        nav.css('opacity', '0');
+        setTimeout(function () {
+            nav.css('display', 'none');
+        }, 300)
+        showMenu = false;
+    })
+    $(window).resize(function () {
+        if ($('body').width() < 992) {
+            nav.css('display', 'none'); 
+            showMenu = false;
+        } else {
+            nav.css('display', 'block');
+            nav.css('opacity', '1');
+            showMenu = false;
+        }
+
+    });
+})
